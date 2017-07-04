24804b9ae0bf916a09154fa5797d88f67d5bc165
FoodleProgedy <foodleprogedy@gmail.com>
2017-07-04 20:20:59 +0200

    Update StaffItem.java

--- src/me/foodle/staffutility/item/StaffItem.java
+++ src/me/foodle/staffutility/item/StaffItem.java
@@ -12,6 +12,28 @@ import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.List;
 
+/*
+
+I realized i can use generics for the execute method.
+Just put an abstract method like so:
+
+public abstract <T extends PlayerEvent> void execute(T event), and in classes that extend it, proceed to check if the instance of that event is the event you want.
+
+
+something like this:
+
+public <T extends PlayerEvent> void execute(T event) {
+     if(event instanceof PlayerInteractEvent) {
+        PlayerInteractEvent e = (PlayerInteractEvent) event;
+        e.getPlayer().sendMessage("Yalla deedle doo");
+     }
+}
+
+Also, this is written in github's edit, so i dunno if this will show any errors while compiling, but it's a simple example.
+
+Yalla deedle doo.
+
+*/
 @Getter
 public abstract class StaffItem {
 
